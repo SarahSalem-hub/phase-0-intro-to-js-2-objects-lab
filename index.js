7ee5cf8cba39ee78371486b37ee401e48eb3cde0
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-23 19:10:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name:"sarah",
+    streetAddress:"earth"
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {...employee,[key]:value}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+     employee[key]=value
+     return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    
+    const newArray = {...employee}
+    delete newArray[key]
+    return newArray
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
